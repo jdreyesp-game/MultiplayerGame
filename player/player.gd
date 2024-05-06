35dd6896ea0face8459c19296f917284427f5b2d
Diego Reyes <jdrp1984@gmail.com>
2024-05-06 11:42:01 +0200

    Initial part of the project as singleplayer

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var player_camera: PackedScene
+@export var camera_height = 316
 @export var player_sprite: AnimatedSprite2D
 
 @export var movement_speed = 300
@@ -10,6 +12,13 @@ extends CharacterBody2D
 @onready var initial_sprite_scale = player_sprite.scale
 
 var jump_count = 0
+var camera_instance
+
+func _ready():
+	set_up_camera()
+
+func _process(_delta):
+	update_camera_pos()
 
 func _physics_process(_delta):
 	
@@ -18,38 +27,59 @@ func _physics_process(_delta):
 	velocity.x = horizontal_input * movement_speed
 	velocity.y += gravity
 	
+	handle_movement_state()
+		
+	move_and_slide()
+	
+	face_movement_direction(horizontal_input)
+
+
+func _on_animated_sprite_2d_animation_finished():
+	player_sprite.play("jump")
+
+func set_up_camera():
+	camera_instance = player_camera.instantiate()
+	camera_instance.global_position.y = camera_height
+	get_tree().current_scene.add_child.call_deferred(camera_instance)
+	
+func update_camera_pos():
+	camera_instance.global_position.x = global_position.x
+
+func face_movement_direction(horizontal_input):
+	if not is_zero_approx(horizontal_input):
+		if horizontal_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		else:
+			player_sprite.scale = initial_sprite_scale
+			
+func handle_movement_state():
+	
 	var is_falling = velocity.y > 0.0 and not is_on_floor()
 	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
 	var is_double_jumping = Input.is_action_just_pressed("jump") and is_falling
 	var is_jump_cancelled = Input.is_action_just_released("jump") and velocity.y < 0.0
 	var is_idle = is_zero_approx(velocity.x) and is_on_floor()
-	var is_walking = is_on_floor() and not is_zero_approx(horizontal_input)
-	
-	if is_jumping:
-		velocity.y = -jump_strength
-		jump_count += 1
-	elif is_double_jumping:
-		velocity.y = -jump_strength
-		jump_count += 1
-	elif is_jump_cancelled:
-		velocity.y = 0.0
-	
-	if is_on_floor():
-		jump_count = 0
-		
-	move_and_slide()
+	var is_walking = is_on_floor() and not is_zero_approx(velocity.x)
 	
 	if is_jumping:
 		player_sprite.play("jump_start")
+	elif is_double_jumping:
+		player_sprite.play("double_jump_start")
 	elif is_walking:
 		player_sprite.play("walk")
 	elif is_falling:
 		player_sprite.play("fall")
 	elif is_idle:
 		player_sprite.play("idle")
-	
-	if not is_zero_approx(horizontal_input):
-		if horizontal_input < 0:
-			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
-		else:
-			player_sprite.scale = initial_sprite_scale
+		
+	if is_jumping:
+		jump_count += 1
+		velocity.y = -jump_strength
+	elif is_double_jumping:
+		jump_count += 1
+		if jump_count <= max_jumps:
+			velocity.y = -jump_strength
+	elif is_jump_cancelled:
+		velocity.y = 0.0
+	elif is_on_floor():
+		jump_count = 0
