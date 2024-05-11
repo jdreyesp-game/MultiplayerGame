230178d15da0921f0b7c93e722d9cac35e68647b
jdiego_reyes <jdiego.reyes@gmail.com>
2024-05-11 17:31:03 +0200

    finished chest

--- interactibles/chest/chest.gd
+++ interactibles/chest/chest.gd
@@ -7,16 +7,14 @@ extends Node2D
 @export var is_locked: bool = true
 
 func activate(state):
-	if not multiplayer.is_server():
-		return
-	
-	is_locked = !state
+	is_locked = false
 	open_chest()
 	
 func open_chest():
 	collider.set_deferred("disabled", true)
-	chest_locked.hide()
-	chest_unlocked.show()
+	if !is_locked:
+		chest_locked.visible = false
+		chest_unlocked.visible = true
 	
 func _on_multiplayer_synchronizer_delta_synchronized():
 	open_chest()
